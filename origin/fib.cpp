771361b000829ccecc40707f1ba2e1e8286dc945
Тульцов Александр <tultsov@inbox.ru>
2022-03-10 20:29:14 +0400

    Fixed Fib Rec

--- origin/fib.cpp
+++ origin/fib.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 unsigned fibRec(int n) {
     if (n == 0) return 0;
-    if (n == 1 || n == 2) return 1;
+    if (n == 1) return 0;
+    if (n == 2 || n == 3) return 1;
 
     return fibRec(n - 1) + fibRec(n - 2);
 }
